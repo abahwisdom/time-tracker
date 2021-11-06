07b99942716ad4f46ee3fc6a13edc7d93a1b16c0
Abah <abahwisdom@gmail.com>
2021-11-06 13:00:43 +0100

    refactor: creacted activities file

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,42 @@
 import ActivityCard from './card';
-import Play from './assets/icon-play.svg'
-import Exercise from './assets/icon-exercise.svg'
-import Social from './assets/icon-social.svg'
-import Study from './assets/icon-study.svg'
-import Work from './assets/icon-work.svg'
-import Selfcare from './assets/icon-self-care.svg'
+import activityArray from './activities'
+// import Play from './assets/icon-play.svg'
+// import Exercise from './assets/icon-exercise.svg'
+// import Social from './assets/icon-social.svg'
+// import Study from './assets/icon-study.svg'
+// import Work from './assets/icon-work.svg'
+// import Selfcare from './assets/icon-self-care.svg'
 import Clock from './assets/icon-clock.svg'
 
 
 
-const activityArray=[
-  {
-    name:'Work', 
-    icon: Work,
-  },
-  {
-    name:'Play', 
-    icon: Play,
-  },
-  {
-    name:'Exercise', 
-    icon: Exercise,
-  },
-  {
-    name:'Social', 
-    icon: Social,
-  },
-  {
-    name:'Selfcare', 
-    icon: Selfcare,
-  },
-  {
-    name:'Study', 
-    icon: Study,
-  },
+// const activityArray=[
+//   {
+//     name:'Work', 
+//     icon: Work,
+//   },
+//   {
+//     name:'Play', 
+//     icon: Play,
+//   },
+//   {
+//     name:'Exercise', 
+//     icon: Exercise,
+//   },
+//   {
+//     name:'Social', 
+//     icon: Social,
+//   },
+//   {
+//     name:'Selfcare', 
+//     icon: Selfcare,
+//   },
+//   {
+//     name:'Study', 
+//     icon: Study,
+//   },
   
-]
+// ]
 
 
 
